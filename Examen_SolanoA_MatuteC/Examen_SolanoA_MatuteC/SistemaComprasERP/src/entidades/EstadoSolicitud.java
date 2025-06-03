04e037db0dcf8010cfd69549bb6759933f3ce4c8
AndresS505-star <sucoshebas@gmail.com>
2025-06-03 18:07:27 -0500

    Update EstadoSolicitud.java

--- Examen_SolanoA_MatuteC/Examen_SolanoA_MatuteC/SistemaComprasERP/src/entidades/EstadoSolicitud.java
+++ Examen_SolanoA_MatuteC/Examen_SolanoA_MatuteC/SistemaComprasERP/src/entidades/EstadoSolicitud.java
@@ -1,8 +1,16 @@
 package entidades;
+// Enumeracion que representara los estados de la solicitud
 
 public enum EstadoSolicitud {
+    //La solicitud fue creada pero no se revisa aun
     SOLICITADA,
+
+    //La solicitud esta siendo revisada por el sistema
     EN_REVISION,
+
+    //La solicitud fue aprobada tras la revision
     APROBADA,
+
+    //La solicitud fue rechazada por la revision
     RECHAZADA
 }
